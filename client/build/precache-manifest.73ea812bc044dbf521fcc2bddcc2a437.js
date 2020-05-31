d6edf9b0dcc83e6d3ad22c2e92705c1cbd634b74
Mansi <mansimaurya0@gmail.com>
2020-06-01 01:56:01 +0530

    'contact-keeper'

---
+++ client/build/precache-manifest.73ea812bc044dbf521fcc2bddcc2a437.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6c870455e9cf699300aa837c09884c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7c1741ee44565994b1c",
+    "url": "/static/css/main.620fe750.chunk.css"
+  },
+  {
+    "revision": "2b5f82b861849f1f1bb8",
+    "url": "/static/js/2.c836a4e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c836a4e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c1741ee44565994b1c",
+    "url": "/static/js/main.e6cd1c81.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
